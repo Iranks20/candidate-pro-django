a3cc93d1bbb75919c60ed6d4cbce32721c0a33af
Iranks20 <rankunda48@gmail.com>
2023-10-13 17:48:05 +0300

    integrated news page and home page plus creating a base template for footer and body

--- mkprofile/urls.py
+++ mkprofile/urls.py
@@ -4,4 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('news/', views.news, name='news'),
+    path('issues/', views.issues, name='issues'),
+    path('events/', views.events, name='events'),
+    path('product-category/', views.productCategory, name='productCategory'),
+    path('product-page/', views.productPage, name='productPage'),
+    path('shop-cart/', views.shopCart, name='shopCart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('myaccount/', views.myAccount, name='myAccount'),
+
+
 ]
--- mkprofile/views.py
+++ mkprofile/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'mkprofile/index.html')
+def news(request):
+    return render(request, 'mkprofile/news_sidebar.html')
+def issues(request):
+    return render(request, 'mkprofile/issues.html')
+def events(request):
+    return render(request, 'mkprofile/event_month.html')
+def productCategory(request):
+    return render(request, 'mkprofile/category_page.html')
+def productPage(request):
+    return render(request, 'mkprofile/product_page.html')
+def shopCart(request):
+    return render(request, 'mkprofile/shop_cart.html')
+def checkout(request):
+    return render(request, 'mkprofile/checkout.html')
+def myAccount(request):
+    return render(request, 'mkprofile/my_account.html')
